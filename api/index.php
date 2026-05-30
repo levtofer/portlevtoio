ef297cc87f4ba7e3438a77874b59ab59e8f7f4cd
Levtofer <levtofer@gmail.com>
2026-05-30 05:16:32 +0000

    changed back index.php (2)

--- api/index.php
+++ api/index.php
@@ -3,4 +3,12 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h2>Error caught!</h2>";
+    echo "<b>Message:</b> " . $e->getMessage() . "<br>";
+    echo "<b>File:</b> " . $e->getFile() . "<br>";
+    echo "<b>Line:</b> " . $e->getLine() . "<br>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
