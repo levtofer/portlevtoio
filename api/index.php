56fb88a90177344732ffff706f59a0527acb4960
Levtofer <levtofer@gmail.com>
2026-05-30 05:15:01 +0000

    changed back index.php

--- api/index.php
+++ api/index.php
@@ -3,22 +3,4 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Test 1: Can PHP run at all?
-echo "PHP is working: " . phpversion() . "<br>";
-
-// Test 2: Can we find the autoloader?
-$autoload = __DIR__ . '/../vendor/autoload.php';
-echo "Autoloader exists: " . (file_exists($autoload) ? 'YES' : 'NO') . "<br>";
-
-// Test 3: Can we find public/index.php?
-$index = __DIR__ . '/../public/index.php';
-echo "public/index.php exists: " . (file_exists($index) ? 'YES' : 'NO') . "<br>";
-
-// Test 4: ENV vars set?
-echo "APP_KEY set: " . (!empty($_ENV['APP_KEY']) ? 'YES' : 'NO') . "<br>";
-echo "APP_ENV: " . ($_ENV['APP_ENV'] ?? 'NOT SET') . "<br>";
-
-echo "ver 1";
-
-// Now try booting Laravel
 require __DIR__ . '/../public/index.php';
